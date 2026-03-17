077f7adb0d2f748ee3585914332623cc91cfe7ee
harshit <1harshitmehendiratta@gmail.com>
2026-03-16 23:48:08 -0500

    Lab1 modified + lab2.ml added.

--- Lab1.ml
+++ Lab1.ml
@@ -1,30 +1,30 @@
 let rec howMany e l =
-    if l = [] then                              *base case*
+    if l = [] then                              (*base case*)
         0
     else if e = List.hd l then
-        1 + howMany e (List.tl l)               *first recursive case*
+        1 + howMany e (List.tl l)               (*first recursive case*)
     else
-        howMany e (List.tl l)                   *second recursive case*
+        howMany e (List.tl l)                   (*second recursive case*)
 
 let rec delete e l =
-    if l = [] then                              *base case*
+    if l = [] then                              (*base case*)
         []
     else if e = List.hd l then
-        delete e (List.tl l)                    *first recursive case*
+        delete e (List.tl l)                    (*first recursive case*)
     else
-        (List.hd l) :: delete e (List.tl l)     *second recursive case*
+        (List.hd l) :: delete e (List.tl l)     (*second recursive case*)
 
 let mean l = 
-    let rec sum lst =                           *helper sum function*
-        if lst = [] then                        *helper sum base case*
+    let rec sum lst =                           (*helper sum function*)
+        if lst = [] then                        (*helper sum base case*)
             0.0
         else
-            List.hd lst +. sum (List.tl lst)    *helper recursive case*
+            List.hd lst +. sum (List.tl lst)    (*helper recursive case*)
     in
-    let rec length lst =                        *helper length function*
-        if lst = [] then                        *helper base case*
-            0.0
+    let rec length lst =                        (*helper length function*)
+        if lst = [] then                        (*helper base case*)
+            0
         else
-            1 + length (List.tl lst)            *helper recursive case*
+            1 + length (List.tl lst)            (*helper recursive case*)
     in
     sum l /. length l
---
+++ Lab2.ml
@@ -0,0 +1,48 @@
+(* Helper functions for rational numbers *)
+let num = fst
+let den = snd
+
+let rec gcd i j =
+    if i <> 0
+        then if j > i
+            then gcd i (j - i)
+            else gcd (i - j) j
+    else j
+
+(* Create a rational number *)
+let rat n d = 
+    let z = gcd n d in
+    (n/z, d/z)
+
+(* Rational number operations *)
+let ratAdd a b = 
+    let newnum = (num a * den b) + (den a * num b) in
+    let newden = (den a * den b) in
+    rat newnum newden
+
+let ratMul a b =
+    let newnum = (num a * num b) in
+    let newden = (den a * den b) in
+    rat newnum newden
+
+let ratDiv a b = 
+    let newnum = (num a * den b) in
+    let newden = (den a * num a) in
+    rat newnum newden
+
+let ratGt a b =
+    (num a * den b) > (num b * den a)
+
+(* Compute the value of Euler's number *)
+let euler () =
+    let eps = rat 1 10000 in
+    let rec calc c s t =
+        if ratGt t eps then
+            let new_s = ratAdd s t in
+            let new_t = ratDiv t c in
+            let new_c = ratAdd c 1 in
+            calc new_c new_s new_t
+        else
+            s
+    in
+    calc (rat 0 1) (rat 0 1) (rat 1 1)
